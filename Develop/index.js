3f06ab8081e0fb10845df57314c57aab40327d88
TraviFravi <travi1500@gmail.com>
2020-12-01 18:15:38 -0500

    Getting started on Questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,31 @@
 // array of questions for user
 const questions = [
+    // title of Project 
+    {
+        type: "input",
+        name: "title",
+        message: "What are you going to title your Project?"
+    
+    },
+    // Description of Project
+    {
+        type: "input",
+        name: "description",
+        message: "What is your Project about?"
+    },
+    // email
+    {
 
+    },
+    // Github Address
+    {
+
+    },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+
 }
 
 // function to initialize program
