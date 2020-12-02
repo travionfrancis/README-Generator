cc271d44bc93eaf2c6f25c376a76db5b50b6b37e
TraviFravi <travi1500@gmail.com>
2020-12-01 20:43:38 -0500

    Finished up the questions and working on the functions

--- Develop/index.js
+++ Develop/index.js
@@ -5,7 +5,6 @@ const questions = [
         type: "input",
         name: "title",
         message: "What are you going to title your Project?"
-    
     },
     // Description of Project
     {
@@ -13,14 +12,38 @@ const questions = [
         name: "description",
         message: "What is your Project about?"
     },
-    // email
+    // Email Address
     {
-
+        type: "input",
+        name: "email",
+        message: "Please Enter Your Email Address"
     },
     // Github Address
     {
-
+        type: "input",
+        name: "user",
+        message: "Please Enter Your Github Username"
     },
+    // Installing Dependecies 
+    {
+        type: "input",
+        name: "installing",
+        message: "What command do you want to use to install dependencies? ex: npm install"
+    },
+    // test run
+    {
+        type: "input",
+        name: "testing",
+        message: "What command should run the Project test? ex: npm test"
+    },
+     // Licenses
+    {
+        type: "list",
+        name: "license",
+        message: "What License will your Project use?",
+        choices: ["Apache License 2.0", "Boost Software License 1.0", "BSD 2-Clause 'Simplified' License","BSD 3-Clause 'New' or 'Revised' License", "Creative Commons Zero v1.0 Universal", "Eclipse Public License 2.0", "GNU Affero General Public License v3.0", "GNU General Public License v2.0", "GNU Lesser General Public License v2.1","GNU General Public License v3.0"]
+    },
+    // Add in more later, just in case
 ];
 
 // function to write README file
