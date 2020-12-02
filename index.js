90686a10fa59686b1c081a57831c320108b8eb84
TraviFravi <travi1500@gmail.com>
2020-12-02 17:18:27 -0500

    Got Node index to finally work

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 // Apparently Needs to have the const dependencies
 const fs = require("fs");
 const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown.js");
 const fileName = "README.md"
 
-// error is coming from this line
-// https://developer.aliyun.com/mirror/npm/package/inquirer-prompt-suggest
-// do this tomorrow
+// Modules
+const api = ("./api")
+const generateMarkdown = require("./utils/generateMarkdown.js");
 inquirer.registerPrompt('suggest', require('inquirer-prompt-suggest'));
+// https://developer.aliyun.com/mirror/npm/package/inquirer-prompt-suggest
+// Look through some docs and other repos and this was the way to
 
 // array of questions for user
 const questions = [
@@ -37,26 +38,29 @@ const questions = [
     },
     // Installing Dependecies 
     {
-        type: "input",
+        type: "suggest",
         name: "installation",
-        message: "What command do you want to use to install dependencies? ex: npm install"
+        message: "What command do you want to use to install dependencies?",
+        suggestions: ["ex: npm install"]
     },
     // test run
     {
         type: "input",
         name: "testing",
-        message: "What command should run the Project test? ex: npm test"
+        message: "What command should run the Project test?",
+        suggestions: ["ex: npm test"]
     },
      // Licenses
+     // https://docs.github.com/en/free-pro-team@latest/github/creating-cloning-and-archiving-repositories/licensing-a-repository
      // May have to change into Checkbox, as my dumb ass wanted to be special and added so many new Licenses.
     {
         type: "list",
         name: "license",
         message: "What License will your Project use?",
         choices: ["Apache License 2.0", "Boost Software License 1.0", "BSD 2-Clause 'Simplified' License","BSD 3-Clause 'New' or 'Revised' License", "Creative Commons Zero v1.0 Universal", "Eclipse Public License 2.0", "GNU Affero General Public License v3.0", "GNU General Public License v2.0", "GNU Lesser General Public License v2.1","GNU General Public License v3.0","Do What F*CK You Want To Public License","ISC","Microsoft Public License","Mozilla Public License",]
-    },
+    }
     // Add in more later, just in case
-    // Also should change the order of these questions soon
+    // Also should change the order of these questions soon if it doesnt make sense.
 ];
 
 // function to write README file
@@ -68,11 +72,11 @@ function writeToFile(fileName, data) {
         // Just in case of an error
         //https://nodejs.org/api/fs.html#fs_fs_writefile_file_data_options_callback
         if (err) {
-            console.log(err);
+            return console.log(err);
         }
         // Make sure if it works, ofc
         console.log("It Worked!");
-    });
+    })
 }
 
 // function to initialize program
